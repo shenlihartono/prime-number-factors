af0763b7f940cd2203fd5c68b3e4f20d0f9b9c21
Shenli Hartono <shenlihartono@gmail.com>
2020-05-09 12:13:23 +0700

    Refactor code

--- src/main/java/com/pany/my/PrimeNumber.java
+++ src/main/java/com/pany/my/PrimeNumber.java
@@ -10,17 +10,15 @@ public class PrimeNumber {
 
     public static List<Integer> factorsOf(int n) {
         List<Integer> result = new ArrayList<>();
-        if (n < 2) {
-            return result;
-        }
+        if (n > 1) {
+            if (n % 2 == 0) {
+                result.add(2);
+                n /= 2;
+            }
 
-        if (n % 2 == 0) {
-            result.add(2);
-            n /= 2;
+            if (n > 1)
+                result.add(n);
         }
-
-        if (n > 1)
-            result.add(n);
 
         return result;
     }
