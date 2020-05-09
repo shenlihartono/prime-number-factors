4337b0185e957eb5753b6f7fcafe9441856bfa45
Shenli Hartono <shenlihartono@gmail.com>
2020-05-09 11:36:26 +0700

    first test: Red

--- src/main/java/com/pany/my/PrimeNumber.java
+++ src/main/java/com/pany/my/PrimeNumber.java
@@ -1,7 +1,13 @@
 package com.pany.my;
 
+import java.util.List;
+
 public class PrimeNumber {
     public static void main(String[] args) {
         System.out.println("hello");
+    }
+
+    public static List<Integer> factorsOf(int n) {
+        return null;
     }
 }
--- src/test/java/com/pany/my/PrimeNumberTests.java
+++ src/test/java/com/pany/my/PrimeNumberTests.java
@@ -2,11 +2,17 @@ package com.pany.my;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PrimeNumberTests {
     @Test
-    void test() {
-        fail();
+    void testPrimeFactors() {
+        List<Integer> expected = new ArrayList<>();
+        List<Integer> actual = PrimeNumber.factorsOf(1);
+        assertThat(actual, equalTo(expected));
     }
 }
