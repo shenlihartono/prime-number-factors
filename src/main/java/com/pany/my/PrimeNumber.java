f9c1db23d0bc006d644474affe7733bc868ac04c
Shenli Hartono <shenlihartono@gmail.com>
2020-05-09 11:26:56 +0700

    init

---
+++ src/main/java/com/pany/my/PrimeNumber.java
@@ -0,0 +1,7 @@
+package com.pany.my;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/com/pany/my/PrimeNumberTests.java
@@ -0,0 +1,12 @@
+package com.pany.my;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PrimeNumberTests {
+    @Test
+    void test() {
+        fail();
+    }
+}
